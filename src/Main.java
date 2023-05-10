520cff80562e49ea6ca4bda928646f6a89611706
kaushalpudasinee <kaushalpudasainee7@gmail.com>
2023-05-10 15:03:30 +0545

    ping pong

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import java.util.Random;
+
+public class Main{
+
+    public static void main(String[] args) {
+
+        GameFrame frame = new GameFrame();
+
+    }
+}
